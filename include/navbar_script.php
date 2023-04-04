fae21508d8d7f25941ddcc01f258b9ce87f3e8ae
lmickael <mickael60000lachevre@gmail.com>
2023-04-04 16:38:18 +0200

    index.html

---
+++ include/navbar_script.php
@@ -0,0 +1,68 @@
+<nav class="navbar navbar-dark bg-dark Sticky-top">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+          <a class="navbar-brand" href="#">The District</a>
+          
+          <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+            <div class="offcanvas-header">
+              <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">bonjours, @username</h5>
+              <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+              <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">connexion/inscription</a>
+                </li>
+                <a class="nav-link" href="#"><span class="reseaux"><i class="fa-sharp fa-solid fa-basket-shopping"></i></span> Panier</a>
+                <li>
+                    <hr class="divider">
+                    <h5>Nos Produit</h5>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Catégories
+                  </a>
+                  <ul class="dropdown-menu dropdown-menu-dark">
+                    <li><a class="dropdown-item" href="#">Nos Menus</a></li>
+                    <li><a class="dropdown-item" href="#">Nos Burgers</a></li>
+                    <li><a class="dropdown-item" href="#">Nos Salades</a></li>
+                    <li><a class="dropdown-item" href="#">Nos Snacks</a></li>
+                    <li><a class="dropdown-item" href="#">Nos Boissons</a></li>
+                    <li><a class="dropdown-item" href="#">Nos Desserts</a></li>
+                </ul>
+                </li>
+                <li>
+                    <hr class="divider">
+                    <h5>espace membre</h5>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">moyen de paiement</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">information personnel</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">mes commandes</a>
+                </li>
+              </ul>
+              <hr class="divider">
+              <h5>Nos réseaux</h5>
+              <span class="reseau">
+                <i class="fa-brands fa-square-facebook"></i>
+                <i class="fa-brands fa-square-instagram"></i>
+                <i class="fa-brands fa-square-twitter"></i>
+            </span>
+            <hr class="divider">
+            <h5>Nous trouver</h5>
+            <p><span class="reseaux"><i class="fa-solid fa-location-dot"></i></span> Adresse: 123 Rue bidons, 80000 Amiens</p>
+            <p><span class="reseaux"><i class="fa-solid fa-mobile-retro"></i></span> 06.00.00.00.00</p>
+            <hr class="divider">
+            <h5>Information</h5>
+            <a href="#" class="nav-link">A Propos</a>
+            <a href="#" class="nav-link">contactez-nous</a>
+        </div>
+          </div>
+        </div>
+      </nav>
